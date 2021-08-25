1c556ad1a53fc75f63d2a9e03ce341efd069dd70
fallen-axe-shay <jerry.allan.akshay@gmail.com>
2021-08-25 10:08:56 -0700

    Got the Node Selection to Work

--- dew-application-virtual/dew-app/src/app/node-link-field/node-link-field.component.ts
+++ dew-application-virtual/dew-app/src/app/node-link-field/node-link-field.component.ts
@@ -8,6 +8,8 @@ import { NODES } from '../nodeLinkList';
   templateUrl: './node-link-field.component.html',
   styleUrls: ['./node-link-field.component.css']
 })
+
+
 export class NodeLinkFieldComponent implements OnInit {
   // nodeLink: NodeLink = {
   //     id: 1,
@@ -20,27 +22,45 @@ export class NodeLinkFieldComponent implements OnInit {
 
   constructor() { 
     this.selectedOption = 'node';
+    this.noOfSelectedOption = 0;
+    this.selectedLabelOption = [];
   }
 
   ngOnInit(): void {
   }
 
   selectedOption?: string;
-  selectedLabelOption?: string;
+  selectedLabelOption?: any;
+  noOfSelectedOption?: any;
 
   onSelect(selection: string): void {
-    switch(selection) {
-      case 'node':
-        this.selectedOption = 'node';
-      break;
-      case 'link':
-        this.selectedOption = 'link';
-      break;
-    }
+    this.selectedOption = selection;
+    // switch(selection) {
+    //   case 'node':
+    //     this.selectedOption = 'node';
+    //   break;
+    //   case 'link':
+    //     this.selectedOption = 'link';
+    //   break;
+    // }
   }
 
   onSelectOption(selection: string): void {
-    this.selectedLabelOption = selection;
+    if(this.selectedLabelOption.includes(selection)) {
+      this.selectedLabelOption.splice(this.selectedLabelOption.indexOf(selection));
+      this.noOfSelectedOption--;
+    } else {
+      switch(this.noOfSelectedOption) {
+        case 2:
+          this.selectedLabelOption[0] = this.selectedLabelOption[1];
+          this.selectedLabelOption[1] = selection;
+          break;
+        default:
+          this.selectedLabelOption.push(selection);
+          this.noOfSelectedOption++;
+          break;
+      }
+    }
   }
 
 }
